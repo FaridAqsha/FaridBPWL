de9138bbca2f8d26ca418c6716f717df4be5ff03
FaridAqsha <85169991+FaridAqsha@users.noreply.github.com>
2021-06-01 19:12:09 +0700

    Add files via upload

---
+++ mvc/view/input.php
@@ -0,0 +1,28 @@
+<h1>CRUD OOP PHP</h1>
+<H2>WWW.MALASNGODING.COM</H2>
+<h3>Edit Data User</h3>
+
+<form action="../controller/proses.php?aksi=tambah" method="post">
+
+<table>
+    <tr>
+        <td>Nama</td>
+        <td>
+            <input type="text" name="nama">
+        </td>
+    </tr>
+    <tr>
+        <td>Alamat</td>
+        <td><input type="text" name="alamat"></td>
+    </tr>
+    <tr>
+        <td>Usia</td>
+        <td><input type="text" name="usia"></td>
+    </tr>
+    <tr>
+    
+        <td></td>
+        <td><input type="submit" value="Simpan"></td>
+
+    </tr>
+    </table>
